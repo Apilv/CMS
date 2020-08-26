0243e7dfd60f676d2c5cf7a6a84d30c8a79d9956
Andrius Pilvelis <andrius.pilv@gmail.com>
2020-08-26 18:32:57 +0300

    ..

--- index.php
+++ index.php
@@ -73,7 +73,7 @@ function generatePage()
                 <head>
                     <meta charset="UTF-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>CMS' . $key->getName() . '</title>
+                    <title>CMS ' . $key->getName() . '</title>
                 </head>
 
                 <body>
